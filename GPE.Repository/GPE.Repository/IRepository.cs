6b3f53ddd1c96c1a7565300db7e64891d95f5b30
Saeed <alalwan@gmail.com>
2016-01-23 13:02:35 -0500

    repository and unitOfWork

--- GPE.Repository/GPE.Repository/IRepository.cs
+++ GPE.Repository/GPE.Repository/IRepository.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace GPE.Repository
 {
-    public interface IRepository<T>: IDisposable
+    public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetList();
-        T GetItem(int id);
-        void Insert(T item);
-        void Delete(int id);
-        void Update(T item);
-        void Save();
+        void Delete(T entityToDelete);
+        void Delete(object id);
+
+        IEnumerable<T> Get(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+
+        T GetByID(object id);
+        void Insert(T entity);
+        void Update(T entityToUpdate);
     }
 }
