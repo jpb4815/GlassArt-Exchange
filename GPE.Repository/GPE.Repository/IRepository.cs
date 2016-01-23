81ce881d5c5f19d2a52044cd7a1c580c88394147
Saeed <alalwan@gmail.com>
2016-01-23 11:54:46 -0500

    initial repository project

---
+++ GPE.Repository/GPE.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPE.Repository
+{
+    public interface IRepository<T>: IDisposable
+    {
+        IEnumerable<T> GetList();
+        T Get(int id);
+        void Insert(T item);
+        void Delete(int id);
+        void Update(T item);
+        void Save();
+    }
+}
